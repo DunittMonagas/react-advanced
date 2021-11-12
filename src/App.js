5661b036dadc3a656f492781f00235c75e98942c
Dunitt Monagas <dunitt@example.com>
2021-11-11 23:35:47 -0400

    Starter overview

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+
 import React from 'react'
+
 function App() {
-  return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
-    </div>
-  )
+	return (
+		<div className='container'>
+			<h2>Advanced React Tutorial</h2>
+		</div>
+	)
 }
 
 export default App
